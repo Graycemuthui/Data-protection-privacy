71df3570cbbb8c2b8874cce582269613561393ed
Grayce Muthui <graycemuthui20@gmail.com>
2023-07-25 18:54:56 +0300

    Add the functionality

--- index.js
+++ index.js
@@ -6,15 +6,66 @@ const app = express();
 const PORT = 8000;
 
 const credentials = {
-  apiKey: "db5a88fd6e9a96f5465befa1ae35db2735c7a0ac06b9557a2a1211abb2988925",
-  username: "Dataisprotected",
+  apiKey: "a414454121d1eabf0bad8d1126a27c9a9e71bb2cbd55418029819c973bed9bba",
+  username: "Dataissafe",
 };
+
 const AfricasTalking = require("africastalking")(credentials);
 const sms = AfricasTalking.SMS;
+let otp = "";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post("/", (req, res) => {
+  const { phoneNumber, text } = req.body;
+
+  console.log(otp, text);
+  if (text === "") {
+    console.log("eeee");
+
+    response = `CON Welcome 
+        1. sign in`;
+  } else if (text === "1") {
+    sendsms();
+    response = `CON Type in the OTP sent to your phone number`;
+  } else if (text !== "1*" + otp) {
+    response = `END Invalid input. Try again`;
+  } else {
+    response = `END you have successfully signed in `;
+  }
+
+  function sendsms() {
+    const AfricasTalking = require("africastalking")(credentials);
+    const sms = AfricasTalking.SMS;
+    otp = Math.floor(100000 + Math.random() * 900000).toString();
+    console.log(otp);
+
+    const message = `Your OTP for 2FA: ${otp}`;
+
+    // Send the SMS
+    const options = {
+      to: phoneNumber,
+      message: message,
+    };
+
+    sms
+      .send(options)
+      .then((response) => {
+        console.log("SMS sent successfully:", response);
+      })
+      .catch((error) => {
+        console.error("Error sending SMS:", error);
+      });
+    return otp;
+  }
+
+  // Print the response onto the page so that our gateway can read it
+  res.set("Content-Type: text/plain");
+
+  res.send(response);
+});
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
