45668ac970fe2c549bc0a0aa4269487fa8a542ec
Grayce Muthui <graycemuthui20@gmail.com>
2023-07-26 16:29:49 +0300

    Add ussd

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const PORT = 8000;
+const PORT = 3000;
+const config = require("./config/development.json");
 
 const credentials = {
-  apiKey: "a414454121d1eabf0bad8d1126a27c9a9e71bb2cbd55418029819c973bed9bba",
-  username: "Dataissafe",
+  apiKey: config.apiKey,
+  username: config.username,
 };
-
 const AfricasTalking = require("africastalking")(credentials);
 const sms = AfricasTalking.SMS;
 let otp = "";
@@ -20,8 +20,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/", (req, res) => {
   const { phoneNumber, text } = req.body;
 
+  console.log("wwwwwww");
   console.log(otp, text);
   if (text === "") {
+    console.log("eeee");
+
     response = `CON Welcome 
         1. sign in`;
   } else if (text === "1") {
@@ -29,14 +32,46 @@ app.post("/", (req, res) => {
     response = `CON Type in the OTP sent to your phone number`;
   } else if (text !== "1*" + otp) {
     response = `END Invalid input. Try again`;
-  } else {
-    response = `END you have successfully signed in `;
+  } else if (text === "1*" + otp) {
+    response = `CON you have successfully signed in!Do you want your parcel delivered? 
+    1.yes:
+    2.no
+    `;
+  }
+  if (text === "1*" + otp + "*1") {
+    console.log(text);
+    response = `CON select your location
+    1. Nairobi
+    2. Mombasa`;
+  }
+  if (text === "1*" + otp + "*2") {
+    console.log(text);
+    response = `END okay thank you for using our services`;
+  }
+  if (text === "1*" + otp + "*1*1") {
+    console.log(text);
+    response = `CON choose your preferred delivery method
+    1. pick up
+    2. drop off`;
+  } else if (text === "1*" + otp + "*1*2") {
+    console.log(text);
+    response = `CON choose your preferred delivery method
+    1. pick up
+    2. drop off`;
+  } else if (text === "1*" + otp + "*1*1*1") {
+    response = `END your parcel will be available for pick up at the nearest post office`;
+  } else if (text === "1*" + otp + "*1*1*2") {
+    response = `END your parcel will be delivered to your doorstep`;
+  } else if (text === "1*" + otp + "*1*2*1") {
+    response = `END your parcel will be available for pick up at the nearest post office`;
+  } else if (text === "1*" + otp + "*1*2*2") {
+    response = `END your parcel will be delivered to your doorstep`;
   }
 
   function sendsms() {
     const AfricasTalking = require("africastalking")(credentials);
     const sms = AfricasTalking.SMS;
-    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    otp = Math.floor(100000 + Math.random() * 900000).toString();
     console.log(otp);
 
     const message = `Your OTP for 2FA: ${otp}`;
